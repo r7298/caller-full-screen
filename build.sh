9d2c220de1ec55d961f5f1c3b3120d2516491bbe
Yury Reshetnikov <reshu@mail.ru>
2021-02-14 11:47:00 +0500

    _

--- build.sh
+++ build.sh
@@ -8,13 +8,14 @@ n=Caller
 
 rm -Rf build
 mkdir -pv build/gen build/apk
-aapt package -f -m -J build/gen -M AndroidManifest.xml -S res -I $aj
+aapt package -f -m -J build/gen -M AndroidManifest.xml -S res -I $aj &&
 javac -source 1.7 -target 1.7 -bootclasspath $rj -classpath $aj \
-      -d build/obj build/gen/$src/R.java java/$src/*.java
-$t/dx --dex --output=build/apk/classes.dex build/obj
+      -d build/obj build/gen/$src/R.java java/$src/*.java &&
+$t/dx --dex --output=build/apk/classes.dex build/obj &&
 aapt package -f -M AndroidManifest.xml -S res -I $aj \
-     -F build/$n.unsigned.apk build/apk
-zipalign -f 4 build/$n.unsigned.apk build/$n.aligned.apk
+     -F build/$n.unsigned.apk build/apk &&
+zipalign -f 4 build/$n.unsigned.apk build/$n.aligned.apk &&
 apksigner sign --ks ~/.hidden/keystore.jks \
 	  --ks-key-alias androidkey --ks-pass pass:android \
-	  --key-pass pass:android --out build/$n.apk build/$n.aligned.apk
+	  --key-pass pass:android --out build/$n.apk build/$n.aligned.apk &&
+ls -l `pwd`/build/$n.apk
